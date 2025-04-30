3dc8da3e7445c86b527d19d431b3d17ef2693e66
Marinaiscodingnow <225mkania@frhsd.com>
2025-04-30 11:46:36 -0400

    IT WORKS

--- script.js
+++ script.js
@@ -1,84 +1,84 @@
 //1
 function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-  
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  }
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+  document.getElementById("overlay").style.display = "none";
+}
 //2
-  function on2() {
-    document.getElementById("second").style.display = "block";
-  }
-  
-  function off2() {
-    document.getElementById("second").style.display = "none";
-  }
-  
-  //3
+function on2() {
+  document.getElementById("second").style.display = "block";
+}
+
+function off2() {
+  document.getElementById("second").style.display = "none";
+}
+
+//3
 function on3() {
-  document.getElementById("third").style.display = "block";
+document.getElementById("third").style.display = "block";
 }
 
 function off3() {
-  document.getElementById("third").style.display = "none";
+document.getElementById("third").style.display = "none";
 }
 
 //4
 function on4() {
-  document.getElementById("fourth").style.display = "block";
+document.getElementById("fourth").style.display = "block";
 }
 
 function off4() {
-  document.getElementById("fourth").style.display = "none";
+document.getElementById("fourth").style.display = "none";
 }
 
 
 
 
 const app = Vue.createApp({
-    created () {
-        fetch('books.json').then(response => response.json()).then(json => {
-              this.books = json
-        })
-  },
-    data() {
-        return {
-            books: [],
+  created () {
+      fetch('books.json').then(response => response.json()).then(json => {
+            this.books = json
+      })
+},
+  data() {
+      return {
+          books: [],
 
-            showBooks: true,
-            title: 'House of Earth and Blood',
-            author: 'Sarah J Mass',
-            pages: 45,
-            total_pages:0
-        }
-    },
-    methods: {
-        toggleShowBooks(){
-            this.showBooks = !this.showBooks
-        }
-    }
+          showBooks: true,
+          title: 'House of Earth and Blood',
+          author: 'Sarah J Mass',
+          pages: 45,
+          total_pages:0
+      }
+  },
+  methods: {
+      toggleShowBooks(){
+          this.showBooks = !this.showBooks
+      }
+  }
 })
 
 app.mount('#app')
 
 
 
-  const celebrateBtn = document.getElementById('celebrateBtn');
+const celebrateBtn = document.getElementById('celebrateBtn');
 
 celebrateBtn.addEventListener('click', () => {
-    // trigger confetti
-    confetti({
-        particleCount: 500,
-        spread: 360,
-        origin: { y: 0.28, }
-    });
+  // trigger confetti
+  confetti({
+      particleCount: 500,
+      spread: 360,
+      origin: { y: 0.28, }
+  });
 
-    // button animation
-    celebrateBtn.style.transform = 'scale(0.95)';
-    setTimeout(() => {
-        celebrateBtn.style.transform = 'scale(1)';
-    }, 100);
+  // button animation
+  celebrateBtn.style.transform = 'scale(0.95)';
+  setTimeout(() => {
+      celebrateBtn.style.transform = 'scale(1)';
+  }, 100);
 });
 
 var slider = document.getElementById("myRange");
@@ -86,6 +86,6 @@ var output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
 slider.oninput = function() {
-  output.innerHTML = this.value;
-  
+output.innerHTML = this.value;
+
 }
